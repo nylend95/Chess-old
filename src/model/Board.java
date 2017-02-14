085df8646a2bf7cb84810fb76adde856b8db0355
Jesper Nylend <jesper.nylend@gmail.com>
2017-02-14 22:26:22 +0100

    Fixing Board, Square and PieceSet

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,17 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jesper Nylend on 10.02.2017.
  * s305070
  */
-public class Board {
-    private int[][] board;
+public class Board
+{
+    private Square[][] board;
+    private PieceSet whitePieceSet;
+    private PieceSet blackPieceSet;
+    ArrayList<Piece> whitePieces;
+    ArrayList<Piece> blackPieces;
 
-    public Board() {
-        board = new int[8][8];
+
+    public Board()
+    {
+        whitePieces = new ArrayList<Piece>(16);
+        blackPieces = new ArrayList<Piece>(16);
+        board = new Square[8][8];
+        whitePieceSet = new PieceSet(whitePieces, "white");
+        blackPieceSet = new PieceSet(blackPieces, "black");
     }
 
-    public int[][] getBoard() {
+    public Square[][] getBoard()
+    {
         return board;
     }
 }
---
+++ src/model/Square.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ * Created by Jesper Nylend on 14.02.2017.
+ * s305070
+ */
+public class Square {
+    private int row;
+    private int column;
+
+    public Square(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
